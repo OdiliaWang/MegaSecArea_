ed4d3f2622e067b9fec22021675becc752ad2003
Ui-odi <odilia.wang@gmail.com>
2024-04-02 14:34:36 +0800

    0402_V3.1

--- assets/js/output.js
+++ assets/js/output.js
@@ -5,7 +5,7 @@ var output = document.getElementById('output');
 /* 點擊事件 */
 button.addEventListener('click', function() {
 /* 在輸出區域內呈現文字 */
-  output.innerHTML = '<div class="alert alert-warning alert-dismissible alert-label-icon label-arrow fade show" role="alert">\
+  output.innerHTML = '<div class="alert alert-success alert-dismissible alert-label-icon label-arrow fade show" role="alert">\
   <i class="fa-solid fa-circle-check label-icon"></i><strong class="fs-18"> 已提出暫停申請成功 </strong>\
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>\
 </div>';
