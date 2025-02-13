cc77c926ffc9d8bd4df127786881b67f522b3959
Ui-odi <odilia.wang@gmail.com>
2025-02-13 11:41:16 +0800

    0213_V3.2

--- assets/js/pages/sweetalerts.custom.js
+++ assets/js/pages/sweetalerts.custom.js
@@ -1,4 +1,4 @@
-//Parameter Custom
+// Parameter Custom
 if (document.getElementById("sa-params"))
     document.getElementById("sa-params").addEventListener("click", function () {
         Swal.fire({
@@ -27,7 +27,7 @@ if (document.getElementById("sa-params"))
         });
     });
 
-//Warning Message
+// Warning Message
 if (document.getElementById("swal2-warning"))
     document.getElementById("swal2-warning").addEventListener("click", function () {
         Swal.fire({
@@ -44,4 +44,74 @@ if (document.getElementById("swal2-warning"))
                 history.back(); // 跳轉回上一頁
             }
         });
+    });
+
+// Long Content
+if (document.getElementById("swl-longcontent"))
+    document.getElementById("swl-longcontent").addEventListener("click", function () {
+        Swal.fire({
+            html: '<div class="card unres-check-card">' +
+                                '<div class="card-header">' +
+                                    '<span class="mb-0 h4">請確認內容明細</span>' +
+                                '</div>' +
+                                '<div class="card-body pb-0">' +
+                                    '<div id="orderCard">' +
+                                        '<div class="card border-1 shadow-none p-1">' +
+                                            '<div class="card-body">' +
+                                                '<div class="row g-3">' +
+                                                    '<div class="col-sm-2">' +
+                                                        '<h5 class="mb-1">台積電</h5>' +
+                                                        '<h5 class="mb-0 text-muted">2330</h5>' +
+                                                    '</div>' +
+                                                    '<div class="col-sm">' +
+                                                        '<h5 class="mb-1 fw-medium">未還張數</h5>' +
+                                                        '<span class="text-secondary">200張</span>' +
+                                                    '</div>' +
+                                                    '<div class="col-sm">' +
+                                                        '<h5 class="mb-1 fw-medium">市值</h5>' +
+                                                        '<span class="text-secondary ">32,500,000元</span>' +
+                                                    '</div>' +
+                                                    '<div class="col-sm">' +
+                                                        '<h5 class="mb-1 fw-medium">本次退還張數</h5>' +
+                                                        '<span class="text-secondary ">1張</span>' +
+                                                    '</div>' +
+                                                '</div>' +
+                                            '</div>' +
+                                        '</div>' +
+                                        '<div class="card border-1 shadow-none p-1">' +
+                                            '<div class="card-body">' +
+                                                '<div class="row g-3">' +
+                                                    '<div class="col-sm-2">' +
+                                                        '<h5 class="mb-1">台積電</h5>' +
+                                                        '<h5 class="mb-0 text-muted">2330</h5>' +
+                                                    '</div>' +
+                                                    '<div class="col-sm">' +
+                                                        '<h5 class="mb-1 fw-medium">未還張數</h5>' +
+                                                        '<span class="text-secondary">200張</span>' +
+                                                    '</div>' +
+                                                    '<div class="col-sm">' +
+                                                        '<h5 class="mb-1 fw-medium">市值</h5>' +
+                                                        '<span class="text-secondary ">32,500,000元</span>' +
+                                                    '</div>' +
+                                                    '<div class="col-sm">' +
+                                                        '<h5 class="mb-1 fw-medium">本次退還張數</h5>' +
+                                                        '<span class="text-secondary ">1張</span>' +
+                                                    '</div>' +
+                                                '</div>' +
+                                            '</div>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>' ,
+            customClass: {
+                confirmButton: 'btn btn-secondary w-xs mt-2',
+                closeButton: 'custom-close-btn'
+            },
+            confirmButtonText: '確認送出',
+            width: 1200,
+            padding: 20,
+            buttonsStyling: false,
+            showCloseButton: true  
+        })
     });
